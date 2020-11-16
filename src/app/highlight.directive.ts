f65e1b6e4ce8609b770df1863355b8b11a8e8657
Nkaka Yves <nkakayves515@gmail.com>
2020-11-16 20:03:29 +0200

    adding highlight directive component

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  constructor(private el: ElementRef) { }
 
-  constructor() { }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('green', 'white');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null, null);
+  }
+
+  private highlight(color: string, textLook: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+    this.el.nativeElement.style.color = textLook;
+  }
 
 }
