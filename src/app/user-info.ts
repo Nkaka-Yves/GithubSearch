3863671918db8f3f8b75bfd749eab058a2b1b35a
Nkaka Yves <nkakayves515@gmail.com>
2020-11-16 11:47:45 +0200

    Creating service Gservice

--- src/app/user-info.ts
+++ src/app/user-info.ts
@@ -1,2 +1,13 @@
 export class UserInfo {
+    constructor(
+        public name:string,
+        public login:string,
+        public location:string,
+        public public_repos: number,
+        public followers:number,
+        public following:number,
+        public avatar_url:string,
+        public created_at:Date,
+        public html_url:string
+    ) {}
 }
