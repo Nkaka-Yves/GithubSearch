b1119608793694c41ce80334b9cb9e71189ea9e7
Nkaka Yves <nkakayves515@gmail.com>
2020-11-16 13:02:34 +0200

    Adding Services component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { UserComponent } from './user/user.component';
 import { ReposComponent } from './repos/repos.component';
+import { HttpClientModule } from "@angular/common/http";
+import { NgProgressHttpClientModule } from "@ngx-progressbar/http-client";
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { FormsModule } from "@angular/forms";
+
+
 
 @NgModule({
   declarations: [
@@ -18,7 +23,11 @@ import { ReposComponent } from './repos/repos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gservice.service.ts
+++ src/app/gservice.service.ts
@@ -1,9 +1,88 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Repos } from './repos';
+import { UserInfo } from './user-info';
+import { environment } from '../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GserviceService {
+ searchUser: UserInfo;
+ repository: Repos;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+     this.searchUser = new UserInfo('','','',0,0,0,'',new Date(),'');
+     this.repository = new Repos('','','','',new Date());
+
+   }
+
+   findUser(searchUser: string){
+     interface apiResponse {
+        name:string;
+        login:string;
+        location:string;
+        public_repos: number;
+        followers:number;
+        following:number;
+        avatar_url:string;
+         created_at:Date;
+         html_url:string;
+        
+    }
+    return new Promise((resolve, reject) => {
+      this.http
+        .get<apiResponse>(
+          'https://api.github.com/users/' +
+            searchUser +
+            '?access_token=' +
+            environment.Key
+        )
+        .toPromise()
+        .then(
+          (response) => {
+            this.searchUser = response;
+            console.log(this.findUser);
+            resolve();
+          },
+          (err) => {
+            console.log(err);
+            reject();
+          }
+        );
+    });
+  }
+
+  getRepository(findRepo: string) {
+    interface apiResponse {
+      name: string;
+      description: string;
+      language: string;
+      html_url: string;
+      updated_at: Date;
+    }
+
+    return new Promise((resolve, reject) => {
+      this.http
+        .get<apiResponse>(
+          'https://api.github.com/users/' +
+            findRepo +
+            '/repos?order=created&sort=asc?access_token=' +
+            environment.Key
+        )
+        .toPromise()
+        .then(
+          (response) => {
+            this.repository = response;
+            console.log(this.repository);
+            resolve();
+          },
+          (err) => {
+            console.log(err);
+            reject();
+          }
+        );
+    });
+  }
 }
