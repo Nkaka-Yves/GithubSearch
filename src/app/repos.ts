ac0031899e26243f753cd9894864cecf949dc4a0
Nkaka Yves <nkakayves515@gmail.com>
2020-11-16 11:10:35 +0200

    Adding class repository component

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -1,2 +1,11 @@
 export class Repos {
+    constructor(
+        public name: string,
+        public description:string,
+        public language:string,
+        public html_url: string,
+        public updated_at:Date
+    ) {} 
+    
 }
+
